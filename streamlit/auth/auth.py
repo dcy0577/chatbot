80a81e5ceab239ec049c0b098ed560f118c6f996
dcy0577 <1227262551@qq.com>
2023-06-05 17:32:43 +0200

    update the path

--- streamlit/auth/auth.py
+++ streamlit/auth/auth.py
@@ -4,7 +4,7 @@ import yaml
 from yaml.loader import SafeLoader
 
 def authenticate():
-    with open('auth/config.yaml') as file:
+    with open('./auth/config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
 
     for key, value in config['credentials']["usernames"].items():
